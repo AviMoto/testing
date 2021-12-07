95cd952901b6d4c7767d815b2bb30dd8230c1f0b
GitHub Action <action@github.com>
2021-12-07 13:31:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/avimoto/testing/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/avimoto/testing/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.avimoto.testing.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.avimoto.testing.MyBundle
 
 class MyApplicationService {
 
